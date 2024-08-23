41efd44200af31df4fc867c876f2a9aa93ebda3f
MaksymPastukh <saponexamq@gmail.com>
2024-08-23 19:04:26 +0200

    Dependency Injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,62 +1,14 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {ProductType} from "./types/product.type";
+import {ProductService} from "./servises/product.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  public products: ProductType[] = [
-    {
-      image: 'product1.png',
-      title: 'Meat Deluxe',
-      subTitle: 'Pepperoni, onions, bacon, tomato paste, sausage, peppers, mushrooms, chili sauce, pineapple.',
-      dateTime:  '2022-11-31  15:00:00'
-    },
-    {
-      image: 'product2.png',
-      title: 'Marine Premium',
-      subTitle: 'Peppers, cheese, shrimp, calamari, mussels, salmon',
-      dateTime:  '2022-12-30  12:00:00'
-    },
-    {
-      image: 'product3.png',
-      title: 'Bacon and Sausage',
-      subTitle: 'Bacon, cheese, sausage, pineapple, tomato paste',
-      dateTime:  '2022-05-28  21:00:00'
-    },
-    {
-      image: 'product4.png',
-      title: 'Chicken Deluxe',
-      subTitle: 'Chicken, pineapple, pepperoni cheese, pizza sauce, tomato paste',
-      dateTime:  '2022-02-05  17:00:00'
-    },
-    {
-      image: 'product5.png',
-      title: 'Barbecue Premium',
-      subTitle: 'BBQ pork, Barbecue sauce, cheese, chicken, pizza sauce, chili sauce',
-      dateTime:  '2022-10-13  19:00:00'
-    },
-    {
-      image: 'product6.png',
-      title: 'Pepperoni Double',
-      subTitle: 'Pepperoni, cheese, 2 types of sausage: fried and cooked',
-      dateTime:  '2022-04-24  12:00:00'
-    },
-    {
-      image: 'product7.png',
-      title: 'Chicken trio',
-      subTitle: 'Fried chicken, Braised chicken, Chicken nuggets, peppers, cheese, mushrooms, pizza sauce',
-      dateTime:  '2022-12-11  15:00:00'
-    },
-    {
-      image: 'product8.png',
-      title: 'Cheese',
-      subTitle: 'Jugas Cheese, Moldy Cheese, Mozzarella Cheese, Secret Cheese.',
-      dateTime:  '2022-06-21  20:00:00'
-    },
-  ]
+export class AppComponent implements OnInit {
+  public products: ProductType[] = []
 
   public formValues = {
     productTitle: '',
@@ -66,12 +18,19 @@ export class AppComponent implements OnInit{
 
   lateData: Promise<string> | null = null
 
+  constructor(private productService: ProductService) {
+
+  }
+
+
   ngOnInit() {
     this.lateData = new Promise<string>(function (resolve) {
       setTimeout(() => {
         resolve('Hello')
       }, 3000)
-    } )
+    })
+
+    this.products = this.productService.getProducts()
   }
 
   public scrollTo(target: HTMLElement): void {
